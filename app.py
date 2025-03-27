5438a062021ce942994d48a76f83a2b3a9cd180c
saurabhgmr <00078989@gmrgroup.in>
2025-03-27 15:55:13 +0530

    main to app

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def get_all_purchasing_groups():
             {"purchasing_group": row["purchasing_group"]} 
             for row in results
             ]
-        return jsonify({"purchasing_groups": purchasing_groupt}), 200
+        return jsonify(purchasing_groupt), 200
     except Exception as e:
         print("Error while fetching materials:", e)
         return jsonify({"error": "An error occurred while fetching 'Purchasing Groups'"}), 500
